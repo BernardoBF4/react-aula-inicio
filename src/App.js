3b0a44c982e23fcdc21d225456cf301a80c8624f
BernardoBF4 <bernardo.fantin@gmail.com>
2021-08-29 21:08:52 -0300

    feat(app): interpolação de variáves com JSX

--- src/App.js
+++ src/App.js
@@ -9,6 +9,19 @@ const joana = {
     { product: 'Guitarra', price: 4000 },
     { product: 'Smartphone', price: 7000 },
   ],
+  active: true,
+};
+
+const mario = {
+  name: 'Mario da Silva',
+  age: 22,
+  purchases: [
+    { product: 'Notebook', price: 2500 },
+    { product: 'Geladeira', price: 300 },
+    { product: 'Guitarra', price: 400 },
+    { product: 'Smartphone', price: 700 },
+  ],
+  active: false,
 };
 
 const App = () => {
@@ -18,12 +31,19 @@ const App = () => {
     .map((item) => item.price)
     .reduce((a, b) => a + b);
 
-  console.log(total_purchases);
-
   return (
     <>
       <p>Nome: {dados.name}</p>
       <p>Idade: {dados.age}</p>
+      <p>
+        Situação:
+        <span style={{ color: dados.active ? 'green' : 'red' }}>
+          {dados.active ? ' Ativa' : ' Inativa'}
+        </span>
+      </p>
+      <p>Total: R$ {total_purchases}</p>
+      {total_purchases > 10000 && <p>Você está gastando muito</p>}
+      {/* O && é um operação que valida se a expressão é true or false; for true, executa o que vem depois, senão, não faz nada */}
     </>
   );
 };
