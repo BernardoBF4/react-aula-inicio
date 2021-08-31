97dd36aade5a53d559abc3430eb73e0cf5913c20
BernardoBF4 <bernardo.fantin@gmail.com>
2021-08-31 19:35:15 -0300

    feat(app): componente de Teste feito com returns corretos

--- src/App.js
+++ src/App.js
@@ -3,9 +3,21 @@ import Header from './Header';
 import Footer from './Footer';
 import Form from './Form/Form';
 
+console.log(React);
+
+const Teste = () => {
+  const active = false;
+  if (active) {
+    return <p>Está ativo</p>;
+  } else {
+    return null;
+  }
+};
+
 const App = () => {
   return (
     <>
+      <Teste />
       <Header />
       Meu App
       <Form />
